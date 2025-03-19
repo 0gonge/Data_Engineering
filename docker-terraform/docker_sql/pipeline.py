808fdc50242668e0b5b8d001e5e9a5f5b8e61e04
0gonge <Challengef0802@gmail.com>
2025-03-20 06:18:21 +0900

    passing Args into the Container

--- docker-terraform/docker_sql/pipeline.py
+++ docker-terraform/docker_sql/pipeline.py
@@ -1,6 +1,10 @@
 #여기가 데이터 파이프라인이 될 것! 
+import sys
 import pandas as pd
 
+print(sys.argv)
+
+day = sys.argv[1]
 # 판다스 했다고 치고
 
-print('job finished successfully')
+print(f'job finished successfully for the day = {day}')
